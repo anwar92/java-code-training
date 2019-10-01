f94f11a625bfc18fe120ef63456617a89b779fe6
Anwar <anwar92@gmail.com>
2019-10-01 17:02:46 -0400

    git commans

---
+++ src/oop/gitHub.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class gitHub {
+    public static void main(String[] args) {
+       /* 1- Initilize
+                git init
+                git remote add origin github_link
+          2- Push code to git hub
+                git add -A
+                git commit -m "message here"
+                git push origin master
+                git status
+        */
+
+    }
+}
